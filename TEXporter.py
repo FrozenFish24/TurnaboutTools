dbde246836977dc4da74f6e061e46da613b89be0
FrozenFish24 <FrozenFish24@users.noreply.github.com>
2015-08-08 01:39:19 +0800

    add argument for forcing .tex files to be interpreted as specified format add slightly imperfect handling for format 16 add stub for format 12 change format unsupported message to format unknown

--- TEXporter.py
+++ TEXporter.py
@@ -9,6 +9,8 @@ def main():
     parser.add_argument('tex', metavar='file.tex', help='the .tex file to be converted')
     parser.add_argument('-o', dest='outfile', help='the file name of the output image file')
     parser.add_argument('-wh', type=int, metavar=('w', 'h'), nargs=2, help='manually specify a width and height')
+    parser.add_argument('-f', type=int, choices=[3, 12, 16, 17],
+                        help='force the .tex file to be interpreted as the specified image type')
     args = parser.parse_args()
 
     if args.outfile is None:
@@ -19,7 +21,11 @@ def main():
         data = f.read()
 
     header = struct.unpack('<9xHBxB6x', header)
-    type = header[2]
+
+    if args.f is None:
+        type = header[2]
+    else:
+        type = args.f
 
     if args.wh is None:
         w = header[0] * 4
@@ -36,6 +42,18 @@ def main():
             a, b, g, r = struct.unpack('BBBB', data[i:i + 4])
             x, y = get_xy(i, 4, image.width, image.height)
             image.putpixel((x, y), (r, g, b, a))
+    # Likely DXT3 or 5
+    elif type == 12:
+        print('Format not implemented')
+        return
+    # 8bpp, 4bit luminance, 4bit alpha(?), non mip-mapped, tiled 2x2 -> 2x2 -> 2x2 (8x8px) [IMPERFECT]
+    elif type == 16:
+        for i in range(0, len(data)):
+            p = struct.unpack('B', data[i])[0]
+            a = ((p << 4) & 0xF0) | (p & 0xF)
+            p = (p & 0xF0) | (p >> 4)
+            x, y = get_xy(i, 1, image.width, image.height)
+            image.putpixel((x, y), (p, p, p, a))
     #  24bpp RGB, non-mipmapped, tiled 2x2 -> 2x2 -> 2x2 (8x8px)
     elif type == 17:
         for i in range(0, len(data), 3):
@@ -43,7 +61,7 @@ def main():
             x, y = get_xy(i, 3, image.width, image.height)
             image.putpixel((x, y), (r, g, b, 255))
     else:
-        print('Format currently unsupported')
+        print('Unknown format, ' + type)
         return
 
     image.save(args.outfile)
